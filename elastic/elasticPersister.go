662914c200456f740c16cd3db9965cff962f27cc
Dino Omanovic <angor.mail@googlemail.com>
2017-04-26 21:48:57 +0200

    Fix merge conflict

--- elastic/elasticPersister.go
+++ elastic/elasticPersister.go
@@ -1,13 +1,20 @@
 package elastic
 
 import (
-	"gopkg.in/olivere/elastic.v5"
 	"context"
+	"gopkg.in/olivere/elastic.v5"
+	"message-persistence-service/common"
 )
+
 const msgIndex string = "messages"
 const msgType string = "message"
 
-func NewElasticClient(ctx context.Context) (*elastic.Client, error) {
+type elasticAdapter struct {
+	client *elastic.Client
+	ctx    context.Context
+}
+
+func NewElasticAdapter(ctx context.Context) (common.PersistenceAdapter, error) {
 	// Create a client
 	client, err := elastic.NewClient(elastic.SetURL("http://elastic:9200"))
 	if err != nil {
@@ -25,5 +32,37 @@ func NewElasticClient(ctx context.Context) (*elastic.Client, error) {
 		}
 	}
 
-	return client, err
+	return &elasticAdapter{client: client, ctx: ctx}, err
+
+}
+
+func (ea *elasticAdapter) Create(msg common.Message) (id string, err error) {
+	resp, err := ea.client.Index().
+		Index(msgIndex).
+		Type(msgType).
+		BodyJson(msg).
+		Do(ea.ctx)
+
+	return resp.Id, err
+}
+
+func (ea *elasticAdapter) Update(id string, msg common.Message) error {
+	_, err := ea.client.Index().
+		Index(msgIndex).
+		Type(msgType).
+		Id(id).
+		BodyJson(msg).
+		Do(ea.ctx)
+
+	return err
+}
+
+func (ea *elasticAdapter) Delete(id string) error {
+	_, err := ea.client.Delete().
+		Index(msgIndex).
+		Type(msgType).
+		Id(id).
+		Do(ea.ctx)
+
+	return err
 }
